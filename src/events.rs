30fc5456f2a812d79e8d7ce3c973752c776cb95e
André Cipriani Bandarra <andreban@gmail.com>
2023-10-28 19:58:31 +0100

    Simplify EventBus

---
+++ src/events.rs
@@ -0,0 +1,32 @@
+use std::{
+    any::{Any, TypeId},
+    collections::HashMap,
+};
+
+type EventHandler = Box<dyn FnMut(&Box<dyn Any>)>;
+
+#[derive(Default)]
+pub struct EventBus {
+    listeners: HashMap<TypeId, Vec<EventHandler>>,
+}
+
+impl EventBus {
+    pub fn add_listener<T: 'static>(&mut self, mut listener: impl FnMut(&T) + 'static) {
+        let type_id = std::any::TypeId::of::<T>();
+        let listeners = self.listeners.entry(type_id).or_default();
+        listeners.push(Box::new(move |e| {
+            let data = e.downcast_ref::<T>().unwrap();
+            listener(data);
+        }));
+    }
+
+    pub fn emit<T: 'static>(&mut self, event: T) {
+        let type_id = std::any::TypeId::of::<T>();
+        if let Some(listeners) = self.listeners.get_mut(&type_id) {
+            let wrapped = Box::new(event) as Box<dyn Any + 'static>;
+            for listener in listeners {
+                listener(&wrapped);
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
 mod asset_manager;
 mod entity;
+pub mod events;
 
 pub mod derive {
     pub use macros::Component;
 }
 
+use std::time::Duration;
+
 pub use asset_manager::AssetManager;
 pub use entity::{get_next_component_type_id, Component, Entity, EntityManager, Query};
+use events::EventBus;
 
 pub struct EntityComponentSystem {
     pub entity_manager: EntityManager,
-    pub systems: Vec<Box<dyn Fn(f32, &AssetManager, &EntityManager)>>,
+    pub systems: Vec<Box<dyn Fn(Duration, &AssetManager, &EntityManager, &mut EventBus)>>,
     pub asset_manager: AssetManager,
+    pub event_bus: EventBus,
 }
 
 impl EntityComponentSystem {
@@ -20,18 +25,27 @@ impl EntityComponentSystem {
             entity_manager: entity::EntityManager::new(),
             systems: Vec::new(),
             asset_manager: AssetManager::default(),
+            event_bus: EventBus::default(),
         }
     }
 
-    pub fn add_system<T: Fn(f32, &AssetManager, &EntityManager) + 'static>(&mut self, system: T) {
+    pub fn add_system<T: Fn(Duration, &AssetManager, &EntityManager, &mut EventBus) + 'static>(
+        &mut self,
+        system: T,
+    ) {
         let boxed = Box::new(system);
         self.systems.push(boxed);
     }
 
-    pub fn update(&mut self, delta_time: f32) {
+    pub fn update(&mut self, delta_time: Duration) {
         self.entity_manager.update();
         for system in &mut self.systems {
-            system(delta_time, &self.asset_manager, &self.entity_manager);
+            system(
+                delta_time,
+                &self.asset_manager,
+                &self.entity_manager,
+                &mut self.event_bus,
+            );
         }
     }
 }
